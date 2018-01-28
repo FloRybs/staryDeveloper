2372a32f0d4691f417108fec8aa2cb5c123f0665
FloRybs <35879984+FloRybs@users.noreply.github.com>
2018-01-28 01:30:26 +0100

    0.1

--- src/staryDeveloper.kt
+++ src/staryDeveloper.kt
@@ -1,10 +1,11 @@
 fun main(args : Array<String>) {
-    val language = if (args.size == 0) "RU" else args[0]
+    val language = if (args.size == 0) "PL" else args[0]
     println(when (language) {
         "EN" -> "Hello!"
         "ES" -> "¡Hola!"
         "RU" -> "Привет!"
         "FR" -> "Salut!"
+        "PL" -> "Siema!"
         else -> "Sorry, I can't greet you in $language yet"
     })
 }
